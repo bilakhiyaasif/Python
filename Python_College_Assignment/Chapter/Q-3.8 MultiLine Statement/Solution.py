ff341ef3eba59677b955d45102311513a2a10876
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-03-18 03:55:02 +0530

    Create Solution.py

---
+++ Python_College_Assignment/Chapter/Q-3.8 MultiLine Statement/Solution.py
@@ -0,0 +1,13 @@
+"""
+MultiLine Statement
+Developer:Bilakhiya Asif
+"""
+
+Name="Asif \n"
+Surname="Bilakhiya \n"
+Designation="Student \n"
+
+MultiLine=	Name+\
+			Surname+\
+			Designation
+print(MultiLine)
