3400385a17c878de0ec94aff2a0632b230f99112
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-03-20 02:13:15 +0530

    Create Solution.py

---
+++ Python_Books_Prablem/ICPS with Python/Chapter 4/4.1.2 Trigonometrix Functions/Solution.py
@@ -0,0 +1,16 @@
+"""
+Trigonometric Functions
+Developer:Bilakhiya Asif
+"""
+import math
+print("math.sin(90)\t\t",math.sin(90))
+print("math.cos(90)\t\t",math.cos(90))
+print("math.tan(90)\t\t",math.tan(90))
+print("math.asin(1)\t\t",math.asin(1))
+print("math.acos(1)\t\t",math.acos(1))
+print("math.atan(1)\t\t",math.atan(1))
+print("math.atan2(3,2)\t\t",math.atan2(3,2))
+print("math.hypot(3,4)\t\t",math.hypot(3,4))
+print("math.degrees(90)\t",math.degrees(90))
+print("math.radians(90)\t",math.radians(90))
+
