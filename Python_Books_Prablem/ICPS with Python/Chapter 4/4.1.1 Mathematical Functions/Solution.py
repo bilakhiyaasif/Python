68549dbb2328b34df5fc48c15f5e810e3d7a9de3
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-03-20 02:01:31 +0530

    Create Solution.py

---
+++ Python_Books_Prablem/ICPS with Python/Chapter 4/4.1.1 Mathematical Functions/Solution.py
@@ -0,0 +1,20 @@
+"""
+Mathematical Functions
+Developer:Bilakhiya Asif
+"""
+import math
+
+print("math.sqrt(25)\t\t",math.sqrt(25))
+print("abs(-120)\t\t",abs(-120))
+print("math.ceil(12.2)\t\t",math.ceil(12.2))
+print("math.floor(12.2)\t",math.floor(12.2))
+print("math.pow(2,3)\t\t",math.pow(2,3))
+print("math.exp(3)\t\t",math.exp(3))
+print("math.fabs(-123)\t\t",math.fabs(-123))
+print("math.log(20)\t\t",math.log(20))
+print("math.log2(31)\t\t",math.log2(31))
+print("math.log10(2)\t\t",math.log10(2))
+print("max(10,4,2,12)\t\t",max(10,4,2,12))
+print("min(10,4,2,12)\t\t",min(10,4,2,12))
+print("round(12.6789,2)\t",round(12.6789,2))
+print("math.modf(12)\t",math.modf(12))
