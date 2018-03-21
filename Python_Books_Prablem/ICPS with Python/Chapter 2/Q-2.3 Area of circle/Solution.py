e1b403f8b7a65293be7996004bffc4be014bf740
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-03-22 02:31:03 +0530

    Create Solution.py

---
+++ Python_Books_Prablem/ICPS with Python/Chapter 2/Q-2.3 Area of circle/Solution.py
@@ -0,0 +1,10 @@
+"""
+find Area of circle
+Area of circle=Pi*(R*R)
+Developer:Bilakhiya Asif
+"""
+import math
+pi=math.pi
+r=input("Enter Radius of Circle:\t")
+r_sq=int(int(r)*int(r))
+print("Area of Circle:\t"+str(float(pi)*r_sq))
