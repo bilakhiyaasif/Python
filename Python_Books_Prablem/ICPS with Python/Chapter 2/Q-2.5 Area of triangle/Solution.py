cf21b1e8f719923c7e3d1f4eb9514b526db3fa28
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-03-22 02:36:14 +0530

    Create Solution.py

---
+++ Python_Books_Prablem/ICPS with Python/Chapter 2/Q-2.5 Area of triangle/Solution.py
@@ -0,0 +1,17 @@
+"""
+find area of triangle
+t=hb/2
+Developer:Bilakhiya Asif
+"""
+print("--------------------------------")
+h=int(input("Enter Height:\t"))
+b=int(input("Enter base:\t"))
+print("--------------------------------")
+
+t=(h*b)/2
+
+
+print("--------------------------------")
+print("Area of Triangle:\t"+str((t)))
+print("--------------------------------")
+
