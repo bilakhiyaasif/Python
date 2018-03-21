3653532dad133d903c82723dd15798feb9a26fab
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-03-22 02:40:28 +0530

    Create Solution.py

---
+++ Python_Books_Prablem/ICPS with Python/Chapter 3/Q-3.8 Quotes in python/Solution.py
@@ -0,0 +1,17 @@
+"""
+Quotes in python
+Developer:Bilakhiya Asif
+"""
+
+word='Asif Bilakhiya'
+sentence="Asif Bilakhiya"
+paragraph="""
+
+Asif Bilakhiya
+Asif Bilakhiya
+
+"""
+
+print(word)
+print(sentence)
+print(paragraph)
