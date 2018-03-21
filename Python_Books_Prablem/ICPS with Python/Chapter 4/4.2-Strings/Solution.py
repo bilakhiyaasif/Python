4bd2cf203a56b7c00cebde5211c879e6d79db43f
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-03-22 05:11:12 +0530

    Create Solution.py

---
+++ Python_Books_Prablem/ICPS with Python/Chapter 4/4.2-Strings/Solution.py
@@ -0,0 +1,15 @@
+"""
+Strings
+Developer:Bilakhiya Asif
+
+"""
+
+s='sololearn';
+print(s)
+print(s[0])
+print(s[11:1500])
+print(s[:4:])
+print(len("data")**int(5))
+print(s[3])
+print(s*2)
+print(s+" hello ")
