0f742c87cc8c4b79cdd83f9f77450f03b437e76e
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-03-22 05:32:45 +0530

    Create Solution.py

---
+++ Python_Books_Prablem/ICPS with Python/Chapter 4/4.2.1 Escape Character/Solution.py
@@ -0,0 +1,15 @@
+"""
+Escape Character
+Developer:Bilakhiya Asif
+
+"""
+print("\a","Bell or Alert")
+print("\b","Backspace")
+print("\f","Formfeed")
+print("\n","NewLine")
+print("\r","Carriage Return")
+print("\s","Space")
+print("\t","Tab")
+print("\v","Vertical Tab")
+
+
