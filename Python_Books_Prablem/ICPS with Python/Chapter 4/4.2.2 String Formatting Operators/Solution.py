1156ef0a75845861f0f21a101132ef192e0fce17
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-03-22 07:08:05 +0530

    Create Solution.py

---
+++ Python_Books_Prablem/ICPS with Python/Chapter 4/4.2.2 String Formatting Operators/Solution.py
@@ -0,0 +1,17 @@
+"""
+Python:String Formating Operators
+Developer:Bilakhiya Asif
+"""
+print("Char \t:%c"%('c'))
+print("String\t:%s"%('String'))
+print("Signed\t:%i"%(-1))
+print("Signed\t:%d"%(1))
+print("Unsigned\t:%u"%(1))
+print("Octal\t:%o"%(8))
+print("HexaDec-Lower\t:%x"%(10))
+print("HexaDec-Upper\t:%X"%(10))
+print("Expo-Lower\t:%e"%(2))
+print("Expo-Upper\t:%E"%(2))
+print("FloatingPoint\t:%f"%(2.5))
+
+
